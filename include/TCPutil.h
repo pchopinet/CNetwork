0d9a7cbb99fe2f81ae1d2e647fdd5e7fefb74dfc
pchopinet <pierre.chopinet97@gmail.com>
2018-08-04 11:00:47 +0200

    Add header files

--- include/TCPutil.h
+++ include/TCPutil.h
@@ -1,9 +1,29 @@
-//
-// Created by chopinet on 19/04/18.
-//
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+*/
 
-#ifndef LIBNETWORK_TCPUTIL_H
-#define LIBNETWORK_TCPUTIL_H
+/**
+ * @author Pierre Chopinet
+ * @Date 2018-08-04
+ */
+
+#ifndef CNETWORK_TCPUTIL_H
+#define CNETWORK_TCPUTIL_H
 
 #include <errno.h>
 #include <sys/socket.h>
@@ -21,4 +41,4 @@ char *TCPrecv(int socketServer);
 
 int TCPclose(int socket);
 
-#endif //LIBNETWORK_TCPUTIL_H
+#endif //CNETWORK_TCPUTIL_H
--- src/TCPutil.c
+++ src/TCPutil.c
@@ -1,6 +1,26 @@
-//
-// Created by chopinet on 19/04/18.
-//
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+*/
+
+/**
+ * @author Pierre Chopinet
+ * @Date 2018-08-04
+ */
 
 #include "include/TCPutil.h"
 
@@ -13,12 +33,12 @@ char *TCPrecv(int socketClient) {
     int size = 0;
     char *msg = malloc(sizeof(char));
     do {
-        recv(socketClient, msg+size, 1, 0);
+        recv(socketClient, msg + size, 1, 0);
         //printf("%c ", msg[size]);
         msg = realloc(msg, sizeof(char) * ++size);
         //printf("%d\n", size);
 
-    } while (msg[size-1] != '\0');
+    } while (msg[size - 1] != '\0');
     msg[size - 1] = '\0';
 
     return msg;
